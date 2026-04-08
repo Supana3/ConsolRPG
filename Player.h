efe096d309ac6332bbe19f7f869974807c610234
Sapana <dhakalsapana28@gmail.com>
2026-04-08 10:45:17 -0700

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,19 @@
+#progma onc
+#include <string>
+#include "Inventory.h"
+
+class Player {
+private:
+    std:: string name;
+    int health;
+    Inventory inventory;
+
+public:
+    Player (std:: string name, int health);
+    ~Player();
+    std:: string getName() const;
+    int getHealth() const;
+    void display() const;
+    void showInventory() const;
+    void addItem(const Item& item);
+};
