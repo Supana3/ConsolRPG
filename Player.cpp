99c30d21872b94473048dd2ab11ab286399afe75
Sapana <dhakalsapana28@gmail.com>
2026-04-08 12:02:31 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,21 +2,24 @@
 #include <iostream>
 
 Player::Player(std::string name, int health)
-   :name(name), health(health), inventory(10)
+   :name(name), health(health), inventory(10), x(0), y(0)
 {
    std::cout << "Default constructor called for Player." << std::endl;
 }
 Player::~Player() {
    std::cout << "Player" << name << "has been destroyed." <<std::endl;
 }
-std::string Player::getName() const {
-  return name;
-}
-int Player::getHealth() const {
-  return health;
-}
+std::string Player::getName() const { return name; }
+int Player::getHealth() const { return health; }
+int Player::getX() const { return x; }
+void Player::setX(int x) {this->x = x; }
+void Player::setY(int y) {this->y = y; }
+
 void Player::display() const {
-  std::cout << "Player;" << name << " | Health: " << health <<std::endl;
+  std::cout << "Player;" << name 
+            << " | Health: " << health
+            << " | Position: (" << x << ", " << y << ")"
+            <<std::endl;
 }
 void Player::showInventory() const {
   inventory.display();
@@ -25,9 +28,7 @@ void Player::addItem(const Item& item) {
   bool added = inventory.addItem(item);
   if (added) {
       std::cout << "An item was added: "<< item.getName() <<std::endl;
-      std::cout << "Hero found a [" << item.getName()
-                << "] (Value; " << item.getValue() << ")" << std::endl;
-  } else {
-       std::cout << "Inventory is full! Could not add: " << item.getName() << std::endl;
+    } else {
+       std::cout << "Inventory is full!" << std::endl;
   }
 }
