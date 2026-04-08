bfe8225df1a0e23b801868d3ddd4fa5365ca919d
Sapana <dhakalsapana28@gmail.com>
2026-04-08 11:02:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ int main() {
 
   Item sword("Sword", 10);
   Item sheild("Sheild", 15);
-  Item potion("Potion", 5); //This one will NOT be added
+  Item potion("Potion", 5); 
+
+  std::cout << "\n--- Adding items to hero's invenypti ---" << std::endl;
+  hero.addItrm(sword);
+  hero.addItem(shield);
 
   std::cout << "\n---Hero's Inventory ---" << std::endl;
   hero.showInventory();
