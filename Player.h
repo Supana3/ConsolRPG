0e7fa588fa4a4251dc7f615d9f6558049ecd92c6
Sapana <dhakalsapana28@gmail.com>
2026-04-08 11:54:22 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -7,12 +7,18 @@ private:
     std::string name;
     int health;
     Inventory inventory;
+    int x;
+    int y;
 
 public:
     Player (std::string name, int health);
     ~Player();
     std::string getName() const;
     int getHealth() const;
+    int getX() const;
+    int getY() const;
+    void setX(int x);
+    void setY(int y);
     void display() const;
     void showInventory() const;
     void addItem(const Item& item);
